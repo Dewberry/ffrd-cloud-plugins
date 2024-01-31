fede7c5cbf2fb8d41b750f36e9d24c6c44599cc6
Anton Kopti <akopti@dewberry.com>
2024-01-31 16:32:29 -0500

    remove unwanted files

--- goprocesses/mcats/README.md
+++
@@ -1,1 +0,0 @@
-# MCATS plugin
--- goprocesses/mcats/update-git-modules.sh
+++
@@ -1,5 +0,0 @@
-# Initialize repository for development
-git submodule update --init --recursive
-
-# Update as needed (changes made to main branch of remote)
-# git submodule update --remote
